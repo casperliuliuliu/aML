ee0358fd03cf7a5aa5016642e93ca00b61fbe7b3
casperliuliuliu <casperliu1118@gmail.com>
2024-03-26 21:58:12 +0800

    seperate the common used function into base_func.py

---
+++ HW3/base_func.py
@@ -0,0 +1,121 @@
+import random
+from torch.utils.data import DataLoader, Subset
+from datetime import datetime
+from tqdm import tqdm
+import numpy as np
+import torch
+from torch import optim, nn
+import time
+def get_dataloaders(train_dataset, val_dataset,test_dataset, transform, train_ratio, val_ratio, batch_size):
+
+    # obtain training indices that will be used for validation
+    num_train = len(test_dataset)
+    indices = list(range(num_train))
+    print("--------- INDEX checking ---------")
+    print(f"Original: {indices[:5]}")
+    random.shuffle(indices)
+    print(f"Shuffled: {indices[:5]}")
+    print("--------- INDEX shuffled ---------\n")
+
+    split_train = int(np.floor(train_ratio * num_train))
+    split_val = split_train + int(np.floor(val_ratio * (num_train-split_train)))
+    train_idx, val_idx, test_idx = indices[0:split_train], indices[split_train:split_val], indices[split_val:]
+    merge_dataset = Subset(train_dataset, train_idx)
+
+    train_loader = DataLoader(merge_dataset, batch_size=batch_size)
+    val_loader = DataLoader(Subset(val_dataset, val_idx), batch_size=batch_size)
+    test_loader = DataLoader(Subset(test_dataset, test_idx), batch_size=batch_size)
+    
+    # check dataset
+    print(f"Total number of samples: {num_train} datapoints")
+    print(f"Number of train samples: {len(train_loader)} batches/ {len(train_loader.dataset)} datapoints")
+    print(f"Number of val samples: {len(val_loader)} batches/ {len(val_loader.dataset)} datapoints")
+    print(f"Number of test samples: {len(test_loader)} batches/ {len(test_loader.dataset)} datapoints")
+    print(f"Data Transform: {transform}")
+    print(f"")
+    
+    dataloaders = {
+        "train": train_loader,
+        "val": val_loader,
+        "test": test_loader,
+    }
+    return dataloaders
+
+
+def pprint(output = '\n', show_time = False): # print and fprint at the same time
+    filename = "hw3-MAR26.txt"
+    print(output)
+    with open(filename, 'a') as f:
+        if show_time:
+            f.write(datetime.now().strftime("[%Y-%m-%d %H:%M:%S] "))
+
+        f.write(str(output))
+        f.write('\n')
+
+def count_parameters(model):
+    total_num = 0
+    for parameter in model.parameters():
+        if parameter.requires_grad:
+            total_num += parameter.numel() 
+    return total_num
+
+def train(model_lists, model_name, loaders, phases = ['train'], save_weight = False):
+    model = model_lists[model_name]()
+    if 'res' in model_name:
+        num_features = model.fc.in_features
+        model.fc = torch.nn.Linear(num_features, 10)
+    elif 'swin' in model_name:
+        num_features = model.head.in_features
+        model.head = torch.nn.Linear(num_features, 10)
+
+    pprint(f"Training model: {model_name}", True)
+    model_parameters_amount = count_parameters(model)
+    pprint(f"Total parameters: {model_parameters_amount:,}")
+
+    model = model.cuda()
+    criterion = nn.CrossEntropyLoss()
+    lr= 0.005
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+    pprint(f"Learning rate={lr}")
+    iteration = 0
+    epochs = 25
+    start = time.time()
+    for epoch in range(epochs):
+        for phase in phases:
+            running_loss = 0.0
+            correct_predictions = 0
+            total_samples = 0
+            if phase == 'train':
+                model.train()  # Set model to training mode
+            else:
+                model.eval()   # Set model to evaluate mode
+            for inputs, labels in tqdm(loaders[phase]): # Iterate over data.
+                inputs, labels = inputs.cuda(), labels.cuda()
+                outputs = model(inputs)
+                loss = criterion(outputs, labels)
+
+                with torch.set_grad_enabled(phase == 'train'):
+                    if phase == 'train': # backward + optimize only if in training phase
+                        optimizer.zero_grad()
+                        loss.backward()
+                        optimizer.step()
+
+                running_loss += loss.item()
+                _, predicted = torch.max(outputs, 1)
+                correct_predictions += (predicted == labels).sum().item()
+                
+
+                total_samples += labels.size(0)
+                iteration += 1
+
+            avg_loss = running_loss / total_samples
+            top1_accuracy = correct_predictions / total_samples * 100
+            pprint(f"Epoch [{epoch+1}/{epochs}], phase: {phase}, samples: {total_samples}, Loss: {avg_loss:.4f}, Top-1 Accuracy: {top1_accuracy:.2f}%")
+    end = time.time()
+    duration = end - start
+    pprint(f"Elapsed time: {duration} seconds")
+    if save_weight:
+        model_scripted = torch.jit.script(model) # Export to TorchScript
+        model_scripted.save(f'{model_name}.pt') # Save
+        pprint(f"weight saved as: {model_name}.pt")
+    return model
